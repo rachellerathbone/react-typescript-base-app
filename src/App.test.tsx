aa1a7871fecd470cc92beaced6b2fc759e624b0c
Rachelle <rachellerathbone@gmail.com>
2021-01-19 11:07:03 +1100

    fix test text

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Renders app', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(
     /Base for React TypeScript app with eslint and prettier./i
   );
+
   expect(linkElement).toBeInTheDocument();
 });
